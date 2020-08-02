78feb1e808b41e74c25fbecd43397ffca91a1360
vishesh1king <visheshvvvm@gmail.com>
2020-08-02 11:41:00 +0530

    Added form and validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+
+  loginForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(25), Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(45), Validators.minLength(8)]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/g),
+                                  Validators.maxLength(16), Validators.minLength(8)])
+  });
+
+  login() {
+    this.loginForm.markAllAsTouched();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { InputComponent } from "./ui-components/input/input.component";
+import { PasswordComponent } from "./ui-components/password/password.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, InputComponent, PasswordComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
