f982952db7780170b589d9d00d1bc20e5d89dda9
Vishesh Raju <visheshvvvm@gmail.com>
2020-08-02 11:22:40 +0530

    New reusable components created

---
+++ src/app/ui-components/password/password.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.css']
+})
+export class PasswordComponent implements OnInit {
+
+
+  constructor(
+    private ref: ChangeDetectorRef
+  ) {
+    this.onBlur = new EventEmitter();
+  }
+
+  @Input() appAutoFocus = false;
+  @Input() formGroup: FormGroup;
+  @Input() control: FormControl;
+  @Input() id = '';
+  @Input() name: string = this.id || '';
+  @Input() placeholder = '';
+  @Input() label = '';
+  @Input() errorMessage: string | boolean = null;
+  @Input() extraClass: string | string[] = '';
+
+  @Input() maxLength = 15;
+  @Input() minLength = 0;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onBlur: EventEmitter<boolean>;
+
+  ngOnInit() { }
+
+  blur() {
+    this.onBlur.emit(true);
+  }
+
+}
